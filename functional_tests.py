e43b498f37d29a470eb3178517e2bed4fb0fc065
gIna Andriyani <gina.andriyani@ui.ac.id>
2015-09-08 14:45:06 +0000

    Modifikasi minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -7,14 +7,15 @@ class NewVIsitorTest(unittest.TestCase):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+#	def tearDown(self):
+#		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
-		
+		self.browser.get('http://localhost:8000')		
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('FInish the test!')
+		header_text=self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Nama saya Gina Andriyani', header_text)
+		self.fail('Finish the test!')
 
 if __name__=='__main__':
 	unittest.main(warnings='ignore')

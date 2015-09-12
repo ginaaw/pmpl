45facb6ce75faa693d3bfddad5b4d3d007114c5c
gIna Andriyani <gina.andriyani@ui.ac.id>
2015-09-12 11:22:14 +0000

    Model for list Items and associated migratuin

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from lists.models import Item
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
@@ -13,16 +14,42 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+
+		#self.assertIn('A new list item', response.content.decode())
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
 		#self.assertTrue(response.content.startswith(b'<html>'))
 		#self.assertIn(b'<title>To-Do lists</title>', response.content)
 		#self.assertTrue(response.content.strip().endswith(b'</html>'))
-		
-#from django.test import TestCase
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
 
-# Create your tests here.
-#class SmokeTest(TestCase):
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		self.assertEqual(response.content.decode(), expected_html)
 
-#	def test_bad_maths(self):
-#		self.assertEqual(1+1, 3)
+class ItemModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+

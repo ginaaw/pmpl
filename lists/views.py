b631036500b45cc959e845267ce8e07859780c87
gIna Andriyani <gina.andriyani@ui.ac.id>
2015-09-23 02:56:57 +0000

    new URL + view for adding to existing lists. FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -11,16 +11,23 @@ def home_page(request):
 	else:
 		komentar = 'oh tidak'
 
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
+	#if request.method == 'POST':
+	#	Item.objects.create(text=request.POST['item_text'])
+	#	return redirect('/lists/the-only-list-in-the-world/')
 	return render(request, 'home.html', {'komentar' : komentar})	
-
-def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items' : items})
 
 def new_list(request):
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/the-only-list-in-the-world/')		
+	return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/%d/' % (list_.id,))
+
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	items = Item.objects.filter(list=list_)
+	return render(request, 'list.html', {'list' : list_})
+

36565292b4bf049b4210cabd0515468b7947aed6
gIna Andriyani <gina.andriyani@ui.ac.id>
2015-11-13 08:12:08 +0000

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -84,25 +84,6 @@ class HomePageTest(TestCase):
 		self.assertTrue(response, self.assertGreater(Item.objects.count(),5))
 		self.assertIn('oh tidak', response.content.decode())
 
-#class ItemModelTest(TestCase):
-	
-#	def test_saving_and_retrieving_items(self):
-#		first_item = Item()
-#		first_item.text = 'The first (ever) list item'
-#		first_item.save()
-
-#		second_item = Item()
-#		second_item.text = 'Item the second'
-#		second_item.save()
-
-#		saved_items = Item.objects.all()
-#		self.assertEqual(saved_items.count(), 2)
-#		
-#		first_saved_item = saved_items[0]
-#		second_saved_item = saved_items[1]
-#		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-#		self.assertEqual(second_saved_item.text, 'Item the second')
-
 class NewListTest(TestCase):
 		
 	def test_saving_a_POST_request(self):
@@ -175,33 +156,4 @@ class ListViewTest(TestCase):
 		other_list = List.objects.create()
 		correct_list = List.objects.create()
 		response = self.client.get('/lists/%d/' % (correct_list.id,))
-		self.assertEqual(response.context['list'], correct_list)	
-class ListAndItemModelTest(TestCase):
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-	
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(),2)
-
-		first_saved_item  = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
-
+		self.assertEqual(response.context['list'], correct_list)
